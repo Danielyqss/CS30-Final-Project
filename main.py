a70e11871777e44a4271de19332ce450f5071b73
XinhaoLiu <89992445+Danielyqss@users.noreply.github.com>
2021-12-01 15:12:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Course: CS30
+# Period: 1
+# Date created: 21/11/29
+# Date last modified: 21/12/01
+# Name: Xinhao Liu
+# Assignment: Final Projecy 
+# Instructor: Janice Cotcher
+
+print("Hi, Thank you for being here\n\nThis is just a little test for your Mental Health \n\nIn this little test you will be doing some Multiple Choice Questions\n\nTo be honest is the key!")
+
+
+
+
+
+
+
+
+
+
